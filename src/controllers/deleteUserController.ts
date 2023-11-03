f7cd42b8d93c34cdee230f401c70b7e377a82d0a
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-11-03 17:07:05 -0300

    fix: adjustment controller delete user and port the server

--- src/controllers/deleteUserController.ts
+++ src/controllers/deleteUserController.ts
@@ -4,11 +4,8 @@ import { DeleteUserService } from "../services/deleteUserService";
 export class DeleteUserController {
     async handle(req: Request, res: Response) {
         const { id } = req.params;
-        console.log(id);
-
         const deleteService = new DeleteUserService();
         const deleted = await deleteService.execute(id);
-
         return res.json(deleted);
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,6 @@ server.use(express.json());
 
 server.use("/", mainRoutes);
 
-const PORT = Number(process.env.PORT) || 4000;
+const PORT = Number(process.env.PORT) || 8080;
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
